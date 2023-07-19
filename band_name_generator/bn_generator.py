c609c6eff138bb379b68a73ab042cda4c66f9116
arifaisal123 <arifaisal123@gmail.com>
2023-07-19 20:56:32 +0600

    initial commit

---
+++ band_name_generator/bn_generator.py
@@ -0,0 +1,11 @@
+# Display initial welcome message to the screen
+print("Welcome to the Band Name Generator.")
+
+# Record city name of the user
+city_name = input("What is the name of the city you grew up in? ")
+
+# Record pet name of the user
+pet_name = input("What is your pet's name? ")
+
+# Display the final band name
+print(f"Your band name could be {city_name} {pet_name}")
